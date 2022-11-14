6f50c42e818248b01cd2e09cf6ebda50fab06a80
ruthvik <ruthvik.nitc@gmail.com>
2022-11-14 16:17:32 -0700

    added parallelization

--- text_recognition_engine_recent.py
+++ text_recognition_engine_recent.py
@@ -2,6 +2,8 @@ import os
 import sys
 import time
 from typing import List, Tuple, Dict
+from functools import partial
+from multiprocessing import Pool
 
 import pytesseract as pytess
 import numpy as np
@@ -10,27 +12,50 @@ import cv2
 from image_utils import image_class as ic
 from image_utils import cv_utils as cu
 from file_utils import file_utils as fu
+from misc_utils import timing_utils as tu
 from file_reader_inference import FileReaderInference
 
 
 class TextRecognitionEngine(object):
-    def __init__(self, images: List, roi_bbox_resize_fracs: Tuple):
+    def __init__(
+        self,
+        images: List,
+        roi_bbox_resize_fracs: Tuple,
+        text_recognition_engine_partial,
+    ):
         self.images = images
         self.roi_bbox_resize_fracs = roi_bbox_resize_fracs  # width_frac, height_frac
         self.recognized_strings = []
+        self.text_recognition_engine_partial = text_recognition_engine_partial
 
+    @tu.timing
     def extract_text_from_images(self):
 
-        t1 = time.time()
         strings = [
             pytess.image_to_string(image, config="--psm 6")  # 6works
             for image in self.images
         ]
-        t2 = time.time()
-        print("Time taken for recog:{}".format(t2 - t1))
-        self.recognized_strings = strings
         # https://stackoverflow.com/questions/60977964/pytesseract-not-recognizing-text-as-expected
-        self.put_text_on_bbox_rois(detected_strings=strings)
+        # self.put_text_on_bbox_rois(detected_strings=strings)
+        self.recognized_strings = strings
+
+    @tu.timing
+    def extract_text_from_images_parallel(self):
+
+        # strings = [
+        #    pytess.image_to_string(image, config="--psm 6")  # 6works
+        #    for image in self.images
+        # ]
+        # https://stackoverflow.com/questions/60977964/pytesseract-not-recognizing-text-as-expected
+        # self.put_text_on_bbox_rois(detected_strings=strings)
+        # im_to_str = partial(pytess.image_to_string, config="--psm 6")
+        pool = Pool(processes=min(len(self.images), 12))
+        # async_results = pool.map_async(func=im_to_str, iterable=self.images)
+        async_results = pool.map_async(
+            func=self.text_recognition_engine_partial, iterable=self.images
+        )
+        strings = async_results.get()
+        self.recognized_strings = strings
 
         return strings
 
@@ -60,9 +85,12 @@ def make_text_recognition_engine(
     else:
         images = [img_obj.image[0] for img_obj in image_objects]
 
+    text_recognition_engine_partial = partial(pytess.image_to_string, config="--psm 6")
+
     text_recognizer_obj = TextRecognitionEngine(
         images=images,
         roi_bbox_resize_fracs=roi_bbox_resize_fracs,
+        text_recognition_engine_partial=text_recognition_engine_partial,
     )
 
     return text_recognizer_obj
@@ -91,7 +119,6 @@ if __name__ == "__main__":
             polygons_path=output_files[i],
             image_path=input_files[i],
         )
-        t1 = time.time()
         file_reader.crop_rois()
         # file_reader.show_rois(visualization_type="polygons")
         # file_reader.show_rois()
@@ -106,12 +133,13 @@ if __name__ == "__main__":
         )
         file_reader.bboxes_rois = (text_recognizer.images, "gray")
 
-        detected_text = text_recognizer.extract_text_from_images()
+        detected_text = text_recognizer.extract_text_from_images_parallel()
+        text_recognizer.put_text_on_bbox_rois(detected_strings=detected_text)
         t2 = time.time()
-        print("Time taken to run one recognition:{}".format(t2 - t1))
-        print(detected_text)
         file_reader.show_rois()
         file_reader.draw_a_samples_polygons()
         file_reader.show_polygons_on_a_sample()
         cv2.waitKey(0)
-        # sys.exit(0)
+    for key, val in tu.TOTAL_TIME.items():
+        print("Average time taken for function {} is {}".format(key, np.mean(val)))
+    # sys.exit(0)
